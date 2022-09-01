0d0628b3d0526db41abe13ccd62ea58537715de4
NBuckle <nikolas.buckle@bison.howard.edu>
2022-09-01 16:20:31 -0400

    This is my first commit for Assignment 1.

---
+++ largescaleprogramming_buckle/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,14 @@
+package org.howard.edu.lsp.assignment1;
+/**
+ * Nikolas Buckle
+ * @author Buckle
+ *
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello. Im Nikolas.");
+
+	}
+
+}
