813f1c93d826badcc9b5d4b1fb3da9bbc2576c04
NBuckle <nikolas.buckle@bison.howard.edu>
2022-09-01 16:32:19 -0400

    Changed text

--- largescaleprogramming_buckle/src/org/howard/edu/lsp/assignment1/HelloWorld.java
+++ largescaleprogramming_buckle/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -7,7 +7,7 @@ package org.howard.edu.lsp.assignment1;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello. Im Nikolas.");
+		System.out.println("Hello Large Scale Programming!!");
 
 	}
 
